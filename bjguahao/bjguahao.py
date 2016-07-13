683000c71905f6650cd54577731fa6823a25bf56
Breaker <791628659@qq.com>
2016-07-13 16:14:08 +0800

    登陆成功

--- bjguahao/bjguahao.py
+++ bjguahao/bjguahao.py
@@ -3,23 +3,66 @@
 
 import urllib
 import urllib2
+import cookielib
 
-domain = "www.bjguahao.gov.cn"
-login_path = "quicklogin.htm"
+USER_AGENT = (
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/45.0.2454.93 Safari/537.36'
+)
 
-def login(mobileNo, password):
-    
-    url = "http://" + domain + "/" + login_path
-    post_data = dict(mobileNo = mobileNo, password = password, yzm = '', isAjax='true')
-    data = urllib.urlencode(post_data)
-    req = urllib2.Request(url, data)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    print the_page
+class APIError(Exception):
+    pass
+class MergeRequest(object):
+    DEFAULT_TITLE = 'Merge Request'
+    DEFAULT_CONTENT = ''
+    def __init__(self, src_branch, dst_branch):
+        self.src_branch = src_branch
+        self.dst_branch = dst_branch
+        self.title = MergeRequest.DEFAULT_TITLE
+        self.content = MergeRequest.DEFAULT_CONTENT
+
+class Client(object):
+
+    def __init__(self):
+        self.domain = "www.bjguahao.gov.cn"
+        self.login_path = "quicklogin.htm"
+
+        self.cookie = cookielib.CookieJar();
+        self.opener = urllib2.build_opener(
+                             urllib2.HTTPCookieProcessor(self.cookie),
+                        )
+
+        self.opener.addheaders = [
+            ('User-Agent', USER_AGENT),
+        ]
+
+    def request(self, method, url, data=None):
+        payload = urllib.urlencode(data) if data else None
+        request = urllib2.Request(url, payload)
+        request.get_method = lambda: method
+        try:
+            response = self.opener.open(request)
+            return response
+        except Exception:
+            raise APIError("unknow error")
+
+       
+    def login(self, mobileNo, password):
+        url = "http://" + self.domain + "/" + self.login_path
+        post_data = dict(mobileNo = mobileNo, password = password, yzm = '', isAjax='true')
+        page = self.request('POST', url, data=post_data)
+        if(page.read() == '{"data":[],"hasError":false,"code":200,"msg":"OK"}'):
+            return True
+        else
+            return False
 
 
 if __name__ == "__main__":
-    login("18510343389", "guahaowwc110")
+    client = Client()
+    client.login("18510343389", "guahaowwc110")
+    client.request("GET", "http://www.bjguahao.gov.cn/index.htm")
+    print client.request("GET", "http://www.bjguahao.gov.cn/u/index.htm").read()
     pass
 
 
